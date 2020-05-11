2b7369cd71b0a156b46768c7fbb5a89ef7670a32
Steffen Köhler <steffen.koehler@yazaki-europe.com>
2020-05-11 12:09:57 +0200

    pivot table seem to work now :-)

--- pdsupport.py
+++ pdsupport.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import numpy
 
 def calculate(mylist):
 	return pd.DataFrame(mylist[1:],columns=mylist[0])
@@ -10,18 +11,42 @@ def calculate(mylist):
 def calculate_pivot(table,layout):
 	headers=table[0]
 	df1 = pd.DataFrame(table[1:],columns=headers)
-	print(df1.head(3))
+	#print(df1.head(3))
+	print(df1)
 	index=[]
-	for row in layout['pivot_rows']:
-		index.append(headers[row-1])
+	pivot_header=['Pivot'] # this array is needed to calculate the header colnmn names for the final calculated pivot table
 	columns=[]
 	for col in layout['pivot_cols']:
 		columns.append(headers[col-1])
+		pivot_header.append(headers[col-1])
+	for row in layout['pivot_rows']:
+		index.append(headers[row-1])
+		pivot_header.append(headers[row-1])
+	pivot_header.append('Value')
 	print(repr(columns))
+	# calculate the aggretor functions
+	aggfunc=[]
+	for agg_func_name in layout['pivot']:
+		if agg_func_name=='min':
+			aggfunc.append(min)
+			continue
+		if agg_func_name=='max':
+			aggfunc.append(max)
+			continue
+		if agg_func_name=='sum':
+			aggfunc.append(sum)
+			continue
+		if agg_func_name=='avg':
+			aggfunc.append(numpy.mean)
+			continue
+		if agg_func_name=='cnt':
+			aggfunc.append(len)
+			continue
+		print("unknown aggregator name {0}".format(agg_func_name))
 	df2= pd.pivot_table(df1, values=headers[layout['pivot_val']-1], 
 						index=index, 
 						columns=columns,
-						aggfunc=[min,max])
+						aggfunc=aggfunc)
 	print(df2)
 	arr = df2.values.tolist()
 	cols = df2.columns.tolist()
@@ -29,7 +54,7 @@ def calculate_pivot(table,layout):
 	print('\nNumpy cols\n----------\n', cols)
 	#print('\nNumpy rows\n----------\n', rows)
 	print('\nNumpy Array\n----------\n', arr)
-	res=[]
+	res=[pivot_header] # add the header line to the result
 	pivot_res=df2.to_dict()
 	for element, element_values in pivot_res.items():
 		for value_name,value in element_values.items():
@@ -37,7 +62,6 @@ def calculate_pivot(table,layout):
 				line=list(element)
 				line.append(value_name)
 				line.append(str(value))
-				print(repr(value))
 				print("\t".join(line))
 				res.append(line)
 	return res
--- sample_table.py
+++ sample_table.py
@@ -43,14 +43,55 @@ t6 = [ # = t2 with column header
 	['Klaus', 'Schulze', 'Berlin', 'am Deich', 9],
 	['Hans', 'Mueller', 'Berlin', 'am Deich', 10],
 ]
+p1 = [ # Pivot test table
+	['Product', 'Day', 'Sold'],
+	['Soap', 'Mo', 8],
+	['Soap', 'Mo', 8],
+	['Water', 'Mo', 2],
+	['Steaks', 'Mo', 4],
+	['Soap', 'Tu', 1],
+	['Water', 'Tu', 1],
+	['Steaks', 'Tu', 7],
+	['Soap', 'We', 2],
+	['Water', 'We', 9],
+	['Steaks', 'We', 7],
+	['Soap', 'Th', 1],
+	['Water', 'Th', 5],
+	['Water', 'Tu', 2],
+	['Salt', 'Th', 6],
+]
+p2 = [ # Pivot test table
+	['Product', 'Day', 'Sold'],
+	['Soap', 'Mo', 5], # changed value here 8 -> 5
+	['Soap', 'Mo', 8],
+	['Water', 'Mo', 2],
+	['Steaks', 'Mo', 4],
+	['Soap', 'Tu', 1],
+	['Water', 'Tu', 1],
+	['Steaks', 'Tu', 9],# changed value here 7 -> 9
+	['Soap', 'We', 2],
+	['Water', 'We', 9],
+	['Steaks', 'We', 7],
+	['Soap', 'Th', 1],
+	['Water', 'Th', 5],
+	['Water', 'Tu', 2],
+	['Pepper', 'Th', 6], # changed Salt to Pepper
+]
+
+debug=False
 
 pts= PyvoStyles('lightgrey','lightgreen','yellow','lightblue', 'aquamarine')
-#pt = Pyvotab(pts, { 'page': 3, 'rows' : [ 3,4 ], 'cols' : [2, 1], 'val' : 5 , 'filter': None, 'pivot': 'plain'}, debug=True)
-pt = Pyvotab(pts, { 'page': 3, 'rows' : [ 2 ], 'cols' : [3], 'val' : 5 , 'filter': None, 'pivot': 'pivot'}, debug=True)
+#pt = Pyvotab(pts, { 'page': 3, 'rows' : [ 3,4 ], 'cols' : [2, 1], 'val' : 5 , 'filter': None, 'pivot': 'plain'}, debug=debug)
+#pt = Pyvotab(pts, { 'page': 3, 'rows' : [ 2 ], 'cols' : [3], 'val' : 5 ,'p_rows' : [ 1,2 ], 'p_cols' : [3], 'p_val' : 4 , 'filter': None, 'pivot': 'pivot'}, debug=debug)
+#pt = Pyvotab(pts, { 'page': 'all', 'rows' : [ 1,2 ], 'cols' : [3], 'val' : 4 ,'p_rows' : [ 2 ], 'p_cols' : [3], 'p_val' : 5 , 'filter': None, 'pivot': 'pivot'}, debug=debug)
 
-pt.InsertTable( t5, False, "white")
-pt.InsertTable( t6, True, "white")
+#pt.InsertTable( t5, False, "white")
+#pt.InsertTable( t6, True, "white")
 
+pt = Pyvotab(pts, { 'page': 'all', 'rows' : [ 3 ], 'cols' : [2], 'val' : 4 ,'p_rows' : [ 1 ], 'p_cols' : [2], 'p_val' : 3 , 'filter': None, 'pivot': 'cnt'}, debug=debug)
+
+pt.InsertTable( p1, False, "white")
+#pt.InsertTable( p2, True, "white")
 
 '''
 rowDepth = pt.headerrows()
@@ -83,7 +124,9 @@ for pyvot_sheet in pt.getPrintDict():
 					print('colspan="',end='')
 				print(str(cell_content["size"])+'" ',end='')
 				print(">", end='')
-
-				print(cell_content["value"]+" row:{0}/col:{1}".format(row,col) +"</td>", end='')
+				if debug:
+					print(cell_content["value"]+" row:{0}/col:{1}".format(row,col) +"</td>", end='')
+				else:
+					print(cell_content["value"] +"</td>", end='')
 		print("</tr>")
 	print("</table>")
